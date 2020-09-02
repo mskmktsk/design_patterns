304d6dfbb7148f6e04bb8d0907e1e36a6d40988e
lc <1261406704@qq.com>
2020-09-02 10:18:10 +0800

    3.7.2 优化代码可读性

--- src/_2_结构型模式/_7_代理模式_Proxy/_2_动态代理/ProxyFactory.java
+++ src/_2_结构型模式/_7_代理模式_Proxy/_2_动态代理/ProxyFactory.java
@@ -1,5 +1,6 @@
 package _2_结构型模式._7_代理模式_Proxy._2_动态代理;
 
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
 public class ProxyFactory {
@@ -10,27 +11,29 @@ public class ProxyFactory {
     }
 
     public Object getProxyInstance() {
-        return Proxy.newProxyInstance(Action.class.getClassLoader(), //target.getClass().getClassLoader()
+        InvocationHandler handler = (proxy, method, args) -> {
+            switch (method.getName()) {
+                case "signin":
+                    System.out.println("登录日志");
+                    break;
+                case "signout":
+                    System.out.println("登出日志");
+                    break;
+                case "register":
+                    System.out.println("注册日志");
+                    break;
+                case "forget":
+                    System.out.println("找回密码日志");
+                    break;
+                default:
+                    System.out.println("其余日志");
+            }
+            Object result = method.invoke(target, args);
+            return result;
+        };
+        return Proxy.newProxyInstance(
+                Action.class.getClassLoader(), //target.getClass().getClassLoader()
                 new Class[] { Action.class } , // target.getClass().getInterfaces();
-                (proxy, method, args) -> {
-                        switch (method.getName()) {
-                            case "signin":
-                                System.out.println("登录日志");
-                                break;
-                            case "signout":
-                                System.out.println("登出日志");
-                                break;
-                            case "register":
-                                System.out.println("注册日志");
-                                break;
-                            case "forget":
-                                System.out.println("找回密码日志");
-                                break;
-                            default:
-                                System.out.println("其余日志");
-                        }
-                    Object result = method.invoke(target, args);
-                    return result;
-                });
+                handler);
     }
 }
